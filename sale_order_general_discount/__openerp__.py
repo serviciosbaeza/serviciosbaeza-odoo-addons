2a353b940b896b80bbb966f791edb2ddf8bccb3f
Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
2012-08-06 18:50:16 +0200

    Añadido módulo sale_order_general_discount

---
+++ sale_order_general_discount/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2012 Servicios Tecnológicos Avanzados (http://www.serviciosbaeza.com)
+#                       Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Sale order general discount',
+    'version': '1.0',
+    'category': 'Generic Modules/Sales & Purchases',
+    'description': """
+    Put a custom discount in all sale order lines.
+    """,
+    'author': 'Pedro Manuel Baeza Romero',
+    'website': 'http://www.serviciosbaeza.com',
+    'depends': ['sale'],
+    'init_xml': [],
+    'update_xml': [
+        'wizard/sale_order_general_discount.xml',
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
