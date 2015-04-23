a25d78217ee595695eee681dc5cf946fe3e37914
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-04-23 09:07:43 +0200

    [IMP] mrp_production_sale_info: Allow to search on sale_order field

--- mrp_production_sale_info/models/mrp_production.py
+++ mrp_production_sale_info/models/mrp_production.py
@@ -38,13 +38,31 @@ class MrpProduction(orm.Model):
             }
         return res
 
+    def _sale_order_search(self, cr, uid, obj, name, args, context=None):
+        field, operator, value = args[0]
+        order_obj = self.pool['sale.order']
+        order_line_obj = self.pool['sale.order.line']
+        if isinstance(value, (int, long)):
+            field = 'id'
+        else:
+            field = 'name'
+        order_ids = order_obj.search(
+            cr, uid, [(field, operator, value)], context=context)
+        order_line_ids = order_line_obj.search(
+            cr, uid, [('order_id', 'in', order_ids)], context=context)
+        procurements = [x.procurement_id for x in order_line_obj.browse(
+            cr, uid, order_line_ids, context=context)]
+        prod_ids = [x.production_id.id for x in procurements
+                    if x.production_id]
+        return [('id', 'in', prod_ids)]
+
     _columns = {
         'partner': fields.function(
             _get_sale_info, type="many2one", relation='res.partner',
             string='Customer', multi=True),
         'sale_order': fields.function(
             _get_sale_info, type="many2one", relation='sale.order',
-            string='Sale Order', multi=True),
+            string='Sale Order', multi=True, fnct_search=_sale_order_search),
         'sale_line': fields.function(
             _get_sale_info, type="many2one", relation='sale.order.line',
             string='Sale Line', multi=True),
