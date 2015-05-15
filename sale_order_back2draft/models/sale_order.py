e759462940acfc2016c46730a36b284fa113e1bb
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-05-15 06:53:04 +0200

    [FIX] sale_order_back2draft: Put in draft also order lines

--- sale_order_back2draft/models/sale_order.py
+++ sale_order_back2draft/models/sale_order.py
@@ -12,12 +12,16 @@ class SaleOrder(orm.Model):
 
     def button_draft(self, cr, uid, ids, context=None):
         wf_service = netsvc.LocalService("workflow")
+        line_obj = self.pool['sale.order.line']
         for order in self.browse(cr, uid, ids, context=context):
             if order.state != 'cancel':
                 raise orm.except_orm(
                     _('Error'),
                     _("You can't back any order that it's not on cancel "
                       "state"))
+            line_obj.write(
+                cr, uid, [x.id for x in order.order_line], {'state': 'draft'},
+                context=context)
             self.write(cr, uid, order.id, {'state': 'draft'}, context=context)
             # Remove and create again the workflow
             wf_service.trg_delete(uid, 'sale.order', order.id, cr)
