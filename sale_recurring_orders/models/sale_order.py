095f7a043e9f00178957d68ff6d0643acbe4f4c4
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-10-17 18:07:57 +0200

    [IMP] sale_recurring_orders: Hooks for agreement creation from order

--- sale_recurring_orders/models/sale_order.py
+++ sale_recurring_orders/models/sale_order.py
@@ -7,29 +7,38 @@ from openerp import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    @api.model
+    def _prepare_agreement_vals(self, order):
+        return {
+            'name': order.name,
+            'partner_id': order.partner_id.id,
+            'company_id': order.company_id.id,
+            'start_date': fields.Datetime.now(),
+        }
+
+    @api.model
+    def _prepare_agreement_line_vals(self, order_line, agreement):
+        return {
+            'agreement_id': agreement.id,
+            'product_id': order_line.product_id.id,
+            'discount': order_line.discount,
+            'quantity': order_line.product_uom_qty,
+        }
+
     @api.multi
     def action_button_generate_agreement(self):
-        agreement_ids = []
+        agreements = []
         agreement_obj = self.env['sale.recurring_orders.agreement']
         agreement_line_obj = self.env['sale.recurring_orders.agreement.line']
         for sale_order in self:
-            agreement = {
-                'name': sale_order.name,
-                'partner_id': sale_order.partner_id.id,
-                'company_id': sale_order.company_id.id,
-                'start_date': fields.Datetime.now(),
-            }
-            agreement_id = agreement_obj.create(agreement)
-            agreement_ids.append(agreement_id)
+            agreement_vals = self._prepare_agreement_vals(sale_order)
+            agreement = agreement_obj.create(agreement_vals)
+            agreements.append(agreement)
             for order_line in sale_order.order_line:
-                agreement_line = {
-                    'agreement_id': agreement_id,
-                    'product_id': order_line.product_id.id,
-                    'discount': order_line.discount,
-                    'quantity': order_line.product_uom_qty,
-                }
-                agreement_line_obj.create(agreement_line)
-        if len(agreement_ids) == 1:
+                agreement_line_vals = self._prepare_agreement_line_vals(
+                    order_line, agreement)
+                agreement_line_obj.create(agreement_line_vals)
+        if len(agreements) == 1:
             # display the agreement record
             view = self.env.ref(
                 'sale_recurring_orders.'
@@ -37,7 +46,7 @@ class SaleOrder(models.Model):
             return {
                 'type': 'ir.actions.act_window',
                 'res_model': 'sale.recurring_orders.agreement',
-                'res_id': agreement_ids[0],
+                'res_id': agreement[0].id,
                 'view_type': 'form',
                 'view_mode': 'form',
                 'view_id': view.id,
@@ -47,5 +56,5 @@ class SaleOrder(models.Model):
         return True
 
     from_agreement = fields.Boolean(
-        string='From agrement?',
+        string='From agreement?', copy=False,
         help='This field indicates if the sale order comes from an agreement.')
